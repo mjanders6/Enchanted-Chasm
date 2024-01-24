dfba740590cd93581e2a5b455327111e0988d56c
Michael J. Anderson <manderson@SMU13006.stmartin.edu>
2024-01-24 12:28:35 -0800

    add functions to create the board, collect wall locations, and set obsticles

--- enchanted-chasm.py
+++ enchanted-chasm.py
@@ -16,12 +16,14 @@ The Cave (20x1 squares) must contain the following: Hero, Walls, Pit, Monster, a
 - The Monster must not be placed within three(3) squares of the Treasure and Pit
 - The Pit must not be placed within two(2) squares from the Treasure
 - The Hero must not be placed a.) within three(3) squares from the Monster and b.) within two(2) squares from the Pit and Treasure
+
 - Hero, Treasure, Pit, and Monster cannot be placed within a Wall
 
 Pseudo Code
     Build the board
     Build  the interrior walls
     Load the walls into the board
+    Create a list of locations on where the walls are
     Generate Random numbers for the following
         4 random inegers + numbers of walls
             Min of 3 walls
@@ -35,18 +37,23 @@ Pseudo Code
     
 List of Functions:
 - Generate board
-- Generate walls
-- Generate random spawn locations
-- Place monster, hero, pit, treasure on the board
-- Move the hero
+* Generate interior walls
+- Build a list where there is a wall
+* Place monster, pit, treasure on the board
+* Generate random spawn locations
+* Move the hero
 
 
 '''
 import random
-#user_wall_choice = int(input('Enter the number of walls needed: '))
-user_wall_choice = 3
 
-# Build Wall
+#user_wall_choice = int(input('Enter the number of walls needed: '))
+user_wall_choice = 1
+
+# random integers used to set locations
+ran_list = random.sample(range(1, 19), 4 + user_wall_choice)
+
+# Build NxN Wall 
 def wall (row, col) :
     my_wall = []
 
@@ -58,8 +65,8 @@ def wall (row, col) :
         j = 0
         while j < col:
             # 
-            if (j == 0) or (j == 19) or (i == 0) or (i == 19):
-                my_wall[i].append('w')
+            if (j == 0) or (j == 19) : # or (i == 0) or (i == 19):
+                my_wall[i].append('W')
             else:
                 my_wall[i].append('E')
             j += 1
@@ -68,13 +75,55 @@ def wall (row, col) :
     return my_wall
 
 # Store a list of locations where a wall is
+def wall_locations (wall) :
+    my_list = []
+    rows = len(wall)
+    i = 0
+    while i < rows:
+        # 
+        my_list.append([])
+        cols = len(wall[i])
+        j = 0
+        while j < cols:
+            if(wall[i][j] == 'W') :
+                my_list[i].append(j)
+            j += 1
+        i += 1
+    return my_list
+
+def set_obstacles (wall) :
+    obstacles_list = ['W','P','T','H', 'M']
+    rows = len(wall)
+    i = 0
+    while i < rows:
+        cols = len(obstacles_list)
+        ran_samp_list = []
+        
+        j = 0
+        while j < cols:
+            ran_sample = random.sample(range(1, 19), 1)[0]
+            
+            if (ran_sample not in ran_samp_list) :
+                ran_samp_list.append(ran_sample)
+                wall[i][ran_sample] = obstacles_list[j]
+            j += 1            
+        i += 1
+
+    return wall
+
+###############
 
 # Function to find the location where the pit, monster,and treasure are
 
 # Generate random locations
 
+# Sets an NxN wall
+my_wall = wall(1, 20)
+set_obstacles(my_wall)
 
-my_wall = wall(20, 20)
+# Creats a list of locations where there is a wall per row
+w_list = wall_locations(my_wall)
+
 
 # Display the matrix
 for row in my_wall:
