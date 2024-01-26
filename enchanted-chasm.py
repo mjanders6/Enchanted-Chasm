22649b37b4e13bbb988c4866e415fadd8916b068
Michael J. Anderson <manderson@SMU13006.stmartin.edu>
2024-01-26 14:53:16 -0800

    created a function to choose a location for an object, updating function to initialize elements

--- enchanted-chasm.py
+++ enchanted-chasm.py
@@ -35,8 +35,7 @@ Pseudo Code
 List of Functions:
 - Generate board
 * Generate interior walls
-- Build a list where there is a wall
-* Define obstales in a dictionary
+- Build a list where there is an obstacles and walls
 * Place monster, pit, treasure on the board
 * Generate random spawn locations
 * Move the hero
@@ -57,8 +56,6 @@ HISTORICAL_MOVEMENTS = []
 
 # Build NxN Wall 
 def game_board (row, col) :
-    #my_wall = []
-
     i = 0
     while i < row:
         # Initialize the inner list for each row
@@ -83,15 +80,16 @@ def obstacle_check(object, threshold=2):
     i = 0
     while (i < len(lstKeys)) :
         if (lstKeys[i] != object) and (lstKeys[i] != 'W'):
+
             list2 = MASTER_OBSTACLES[lstKeys[i]]
             print(i, lstKeys[i])
+
             if len(list1) != len(list2):
                 return "Lists are not of the same length."
 
             for point1, point2 in zip(list1, list2):
                 x1, y1 = point1
                 x2, y2 = point2
-
 
                 if abs(x1 - x2) <= threshold and abs(y1 - y2) <= threshold:
                     return True
@@ -101,34 +99,36 @@ def obstacle_check(object, threshold=2):
         i += 1
 
 
+
 # Store a list of locations where a wall is
-def obstacle_locations (wall) :
-    #my_list = []
-    rows = len(wall)
+def obstacle_locations () :
+    MASTER_OBSTACLE_LOCATIONS.clear()
+    rows = len(MASTER_BOARD)
     i = 0
     while i < rows:
         # 
         MASTER_OBSTACLE_LOCATIONS.append([])
-        cols = len(wall[i])
+        cols = len(MASTER_BOARD[i])
         j = 0
         while j < cols:
-            if (wall[i][j] == 'W') or (wall[i][j] == 'P') or (wall[i][j] == 'T') or (wall[i][j] == 'M') or (wall[i][j] == ' '):
+            if (MASTER_BOARD[i][j] == 'W') or (MASTER_BOARD[i][j] == 'P') or (MASTER_BOARD[i][j] == 'H') or (MASTER_BOARD[i][j] == 'T') or (MASTER_BOARD[i][j] == 'M') or (MASTER_BOARD[i][j] == ' '):
                 MASTER_OBSTACLE_LOCATIONS[i].append(j)
             j += 1
         i += 1
     return MASTER_OBSTACLE_LOCATIONS
 
 # Catalog empty locations
-def spawn_locations (wall) :
-    rows = len(wall)
+def spawn_locations () :
+    MASTER_SPAWN_LOCATIONS.clear()
+    rows = len(MASTER_BOARD)
     i = 0
     while i < rows:
         #
         MASTER_SPAWN_LOCATIONS.append([])
-        cols = len(wall[i])
+        cols = len(MASTER_BOARD[i])
         j = 0
         while j < cols:
-            if (wall[i][j] == 'E') :
+            if (MASTER_BOARD[i][j] == 'E') :
                 MASTER_SPAWN_LOCATIONS[i].append(j)
             j += 1
         i += 1
@@ -136,10 +136,11 @@ def spawn_locations (wall) :
 
 # Debug, clean view, to show where the obstacles are
 def debug_spawn_locations () :
+    DEBUG_SPAWN_LOCATIONS.clear()
     rows = len(MASTER_BOARD)
 
     if len(DEBUG_SPAWN_LOCATIONS) > 0:
-        DEBUG_SPAWN_LOCATIONS.clear()
+
         i = 0
         while i < rows:
             #
@@ -171,21 +172,36 @@ def debug_spawn_locations () :
     return DEBUG_SPAWN_LOCATIONS
 
 # Initialize the board with obstacles
-def initialize_obstacle_location (board) :
-    obstacles_list = ['W','P','T', 'M']
-    rows = len(board) - 1
-    cols = len(board[0]) - 1
+def initialize_obstacle_location () :
+    # initialize obstacles list
+    # initialize # of row/# of column
+    # initiate first obstacle
+    # create location for next object from list of spawnable locations
+        # If M -> threshhold is 3
+        # If not M -> threshhold is 2
+    #
+    #
+    #
+    MASTER_OBSTACLES.clear()
+    obstacles_list = ['W','P', 'M']
+    rows = len(MASTER_BOARD) - 1
+    cols = len(MASTER_BOARD[0]) - 1
 
-    # MASTER_OBSTACLES.update({'H' : [ran_row, ran_col]})
+    ran_row = random.sample(range(1, rows), 1)[0]
+    ran_col = random.sample(range(1, cols), 1)[0]
+
+    MASTER_OBSTACLES.update({'T' : [(ran_row, ran_col)]})
+    MASTER_BOARD[ran_row][ran_col] = 'T'
 
     i = 0 
     while i < len(obstacles_list) :
         ran_row = random.sample(range(1, rows), 1)[0]
         ran_col = random.sample(range(1, cols), 1)[0]
         # Enforce the rules for obstacle locations
-        MASTER_OBSTACLES.update({obstacles_list[i] : [(ran_row, ran_col)]})
 
-        MASTER_BOARD[ran_row][ran_col] = obstacles_list[i]
+        #MASTER_OBSTACLES.update({obstacles_list[i] : [(ran_row, ran_col)]})
+
+        #MASTER_BOARD[ran_row][ran_col] = obstacles_list[i]
         
         i += 1
 
@@ -204,8 +220,6 @@ def spawn_hero () :
         ran_row = random.sample(range(1, rows), 1)[0]
 
         ran_col = random.choice(MASTER_SPAWN_LOCATIONS[ran_row])
-        #ran_col = random.sample(range(1, cols), 1)[0]
-        # Enforce the rules for obstacle locations
         MASTER_OBSTACLES.update({heros_list[i]: [(ran_row, ran_col)]})
 
         MASTER_BOARD[ran_row][ran_col] = heros_list[i]
@@ -213,13 +227,37 @@ def spawn_hero () :
         i += 1
     return MASTER_OBSTACLES
 
+# Spawn an object H, M, W, T, P
+def spawn_object (object) :
+
+    rows = len(MASTER_SPAWN_LOCATIONS) - 1
+
+    current_obj_row = MASTER_OBSTACLES[object][0][0]
+    current_obj_col = MASTER_OBSTACLES[object][0][1]
+    MASTER_BOARD[current_obj_row][current_obj_col] = 'E'
+
+
+    ran_row = random.sample(range(1, rows), 1)[0]
+    ran_col = random.choice(MASTER_SPAWN_LOCATIONS[ran_row])
+
+    MASTER_OBSTACLES.update({ object : [(ran_row, ran_col)]})
+
+
+    MASTER_BOARD[ran_row][ran_col] = object
+
+    obstacle_locations()
+    debug_spawn_locations()
+    spawn_locations()
+
+
+
 
 # Initialize the game
 def initialze_board () :
-    game_board(5,20)
-    initialize_obstacle_location(MASTER_BOARD)
-    obstacle_locations(MASTER_BOARD)
-    spawn_locations(MASTER_BOARD)
+    game_board(3, 20)
+    initialize_obstacle_location()
+    obstacle_locations()
+    spawn_locations()
     spawn_hero()
     debug_spawn_locations()
     #obstacle_check('M')  # just debugging
