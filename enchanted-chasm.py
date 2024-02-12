49e2059e12a0f34deda13ffdef6cc8d0507a144f
mjanders6 <mjanders6@gmail.com>
2024-02-11 21:44:08 -0800

    text box class

--- enchanted-chasm.py
+++ enchanted-chasm.py
@@ -137,6 +137,25 @@ def game_gui():
     text.place(x=5, y=60)
     # text.insert('1.0', Game_Board.GAME_TEXT)
 
+    '''
+    from tkinter import *
+from tkinter import ttk
+
+root = Tk()
+log = Text(root, state='disabled', width=80, height=24, wrap='none')
+log.grid()
+
+def writeToLog(msg):
+    numlines = int(log.index('end - 1 line').split('.')[0])
+    log['state'] = 'normal'
+    if numlines==24:
+        log.delete(1.0, 2.0)
+    if log.index('end-1c')!='1.0':
+        log.insert('end', '\n')
+    log.insert('end', msg)
+    log['state'] = 'disabled'
+    '''
+
     center_frame = Frame(
         root,
         bg='white',
