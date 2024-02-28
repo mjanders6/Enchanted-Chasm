b6dea3ce9034a1f3fd03c41e404d088239b18cde
Michael J. Anderson <manderson@SMU13006.stmartin.edu>
2024-02-28 10:02:06 -0800

    updating backend process

--- Class/backend.py
+++ Class/backend.py
@@ -2,12 +2,46 @@ import json
 
 class back_end:
     def __init__(self):
-        self.data = ''
+        pass
 
-    # load file
 
-    # save file
+    @staticmethod
+    def get_data(db_file):
+        with open(db_file) as json_file:
+            # Reading from json file
+            db = json.load(json_file)
+            return db
 
-    # create file if created already
+    @staticmethod
+    # save data
+    def save_data(file, db):
+        with open(file, 'w') as json_file_out:
+            json.dump(db, json_file_out)
 
-    # get
+    # read in game board file and save to the MASTER_BOARD list in the db and save the file
+    @staticmethod
+    def init_board(file, json_file):
+        db = back_end.get_data(json_file)
+
+        with open(file, 'r') as file:
+            # Iterate over each line in the file
+            for line in file:
+                # Split the line into fields based on whitespace
+                fields = line.strip().split()
+                db['MASTER_BOARD'].append(fields)
+            file.close()
+        back_end.save_data(json_file, db)
+
+    # read in
+
+
+# initialize file and database
+file = "chasmDB.json"
+game_file = 'TheCave.txt'
+db = back_end.get_data(file)
+
+# db['MASTER_BOARD'].append([1,2,3,4,5])
+# back_end.save_data(file, db)
+
+
+
